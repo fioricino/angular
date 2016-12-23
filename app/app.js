ab9d62b6aa4895391bb607cbcedd74ba7accc96e
ozavorot <Olga.Zavorotkina@t-systems.com>
2016-12-23 15:37:43 +0300

    dev

--- app/app.js
+++ app/app.js
@@ -5,54 +5,11 @@ app.controller('NewsController', [function() {
     this.selectedNew = null;
 
     this.isSelected = function (topic) {
-        return this.currentTopic === topic;
+        return this.currentTopic == topic;
     };
 
-    this.news = [
-        {
-            topic: "World",
-            header: "Some news",
-            text: "This is some news",
-            summary: "This is some summary",
-            comments: [
-                {
-                    text: "This is some comment",
-                    author: "joe@mail.ru",
-                    date: "01.01.2016"
-                }
-            ]
-        },
-        {
-            topic: "Sport",
-            header: "Some sport news",
-            text: "This is some sport news",
-            summary: "This is some sport summary"
-        },
-        {
-            topic: "Weather",
-            header: "Some weather news",
-            text: "This is some weather news",
-            summary: "This is some weather summary"
-        },
-        {
-            topic: "World",
-            header: "Some news #2",
-            text: "This is some news #2",
-            summary: "This is some world summary #2"
-        },
-        {
-            topic: "World",
-            header: "Another world news",
-            text: "This is another world news",
-            summary: "This is another world summary"
-        },
-        {
-            topic: "Sport",
-            header: "Another sport news",
-            text: "This is another sport news",
-            summary: "This is some sport summary"
-        }
-    ];
+    this.news = data;
+
     this.selectTopic = function(topic) {
         this.currentTopic = topic;
         this.filteredNews = this.news.filter(function(n) {
@@ -92,6 +49,13 @@ app.directive("commentInput", function() {
     return {
         restrict: 'E',
         templateUrl: 'comment-input.html'
+    };
+});
+
+app.directive("comment", function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'comment.html'
     };
 });
 
---
+++ app/news.js
@@ -0,0 +1,45 @@
+data = [
+    {
+        topic: "World",
+        header: "Some news",
+        text: "This is some news",
+        summary: "This is some summary",
+        comments: [
+            {
+                text: "This is some comment",
+                author: "joe@mail.ru",
+                date: "01.01.2016"
+            }
+        ]
+    },
+    {
+        topic: "Sport",
+        header: "Some sport news",
+        text: "This is some sport news",
+        summary: "This is some sport summary"
+    },
+    {
+        topic: "Weather",
+        header: "Some weather news",
+        text: "This is some weather news",
+        summary: "This is some weather summary"
+    },
+    {
+        topic: "World",
+        header: "Some news #2",
+        text: "This is some news #2",
+        summary: "This is some world summary #2"
+    },
+    {
+        topic: "World",
+        header: "Another world news",
+        text: "This is another world news",
+        summary: "This is another world summary"
+    },
+    {
+        topic: "Sport",
+        header: "Another sport news",
+        text: "This is another sport news",
+        summary: "This is some sport summary"
+    }
+];
