76156cc1c547fd6d34a1f8aae59457a4ccb79504
ozavorot <Olga.Zavorotkina@t-systems.com>
2016-12-24 21:43:34 +0300

    dev

--- app/news.js
+++ app/news.js
@@ -4,6 +4,7 @@ data = [
         header: "Some news",
         text: "This is some news",
         summary: "This is some summary",
+        image: "test.png",
         comments: [
             {
                 text: "This is some comment",
@@ -20,9 +21,15 @@ data = [
     },
     {
         topic: "Weather",
-        header: "Some weather news",
-        text: "This is some weather news",
-        summary: "This is some weather summary"
+        header: "Storm on Philippines.",
+        text: "MANILA, Philippines –  A tropical storm nearing the Philippines on Friday may strengthen into a typhoon, bringing moderate to heavy rains and blustery weather on Christmas Day, the biggest holiday in the predominantly Roman Catholic nation.\
+        Tropical Storm Nock-Ten was 790 kilometers (490 miles) east of Guiuan town in the eastern part of the country as of Friday morning, packing winds of up to 105 kilometers per hour (65 miles per hour) and gusts of up to 130 kph (80 mph), forecasters said.\
+    Landrico Dalida Jr., a deputy administrator of the government weather bureau, said Nock-Ten may intensify into a typhoon, with winds of at least 120 kph (75 mph), before it makes landfall late Sunday in the eastern part of the country.\
+\"Our Christmas will be rainy, with strong winds,\" Dalida said. \"So while we celebrate, let us also prepare.\"\
+Officials warned of possible landslides and flooding along the storm's path, and storm surges in coastal areas that makes sea travel risky. Stormy weather can be expected starting Saturday evening, they said.\
+Nock-Ten is forecast to move northwest over northern provinces. It is expected to be over the country's capital on Monday and to exit the Philippines by Wednesday.",
+        summary: "Philippines warned of rainy Christmas from tropical storm.",
+        image: "storm.png"
     },
     {
         topic: "World",
